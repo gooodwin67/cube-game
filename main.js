c5649238e9fe14ed2cc9d315377fb06f700ce23c
Light Course <sn67@inbox.ru>
2024-07-31 16:34:41 +0300

    data

--- main.js
+++ main.js
@@ -2,12 +2,27 @@ import * as THREE from 'three';
 import Stats from 'three/addons/libs/stats.module.js';
 
 import { detectCollisionCubes } from './detectColisions.js';
+import { MathUtils } from 'three';
 
 import { OrbitControls } from "three/addons/controls/OrbitControls";
 
 
-let clock = new THREE.Clock();
+
+
 let stats;
+
+let mouse = new THREE.Vector2();
+const target = new THREE.Vector3();
+
+let fieldSize = 110;
+let fieldDivisions = 11;
+
+let movementX = 0;
+let movementY = 0;
+let rotationSpeed = 5;
+
+var plane;
+let player;
 
 
 let scene = new THREE.Scene();
@@ -15,12 +30,14 @@ let scene = new THREE.Scene();
 let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(innerWidth, innerHeight);
 renderer.setClearColor(0xffffff)
-//renderer.shadowMap.enabled = true;
-//renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.gammaInput = true;
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener("resize", (event) => {
+  // windowHalfX = window.innerWidth / 2;
+  // windowHalfY = window.innerHeight / 2;
   camera.aspect = innerWidth / innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(innerWidth, innerHeight);
@@ -30,10 +47,9 @@ stats = new Stats();
 document.body.appendChild(stats.dom);
 
 let camera = new THREE.PerspectiveCamera(30, innerWidth / innerHeight, 1, 1000);
-camera.position.set(50, 50, 0);
-//camera.lookAt(0,0,0);
+camera.position.set(0, 160, 180);
+camera.lookAt(0, 0, 0);
 
-//var ambient = new THREE.AmbientLight( 0x404040);
 var ambient = new THREE.AmbientLight(0xcccccc, 0.4);
 scene.add(ambient)
 
@@ -55,32 +71,38 @@ dirLight.shadow.bias = 0.01;
 scene.add(dirLight);
 
 
-let controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.target.set(0, 5, 0);
+// let controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.target.set(0, 0, 0);
 
 
-
-var geometryPlane = new THREE.BoxGeometry(30, 1, 30);
-var materialPlane = new THREE.MeshPhongMaterial({ color: 0xcccccc, side: THREE.DoubleSide })
-var plane = new THREE.Mesh(geometryPlane, materialPlane);
-//plane.receiveShadow = true;
-plane.position.set(0, -0.5, 0);
+var geometryPlane = new THREE.BoxGeometry(fieldSize, 1, fieldSize);
+var materialPlane = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide })
+plane = new THREE.Mesh(geometryPlane, materialPlane);
+plane.receiveShadow = true;
+plane.position.set(0, 0, 0);
 scene.add(plane);
 
-const size = 1000;
-const divisions = 100;
+const gridHelper = new THREE.GridHelper(fieldSize, fieldDivisions, 0x0000ff, 0x808080);
+gridHelper.position.y = 0.6;
+gridHelper.position.x = 0;
+scene.add(gridHelper);
 
 
-// const fieldGeometry = new THREE.BufferGeometry(200, 200, 200);
-// const fieldMaterial = new THREE.MeshLambertMaterial({ color: 0xaca181 });
+const geometryPlayer = new THREE.CylinderGeometry(0, 2, 10, 4);
+geometryPlayer.rotateX(Math.PI / 2);
+const materialPlayer = new THREE.MeshPhongMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
+player = new THREE.Mesh(geometryPlayer, materialPlayer);
+player.castShadow = true;
+player.position.y = 3;
 
-// let fieldMesh = new THREE.Mesh(fieldGeometry, fieldMaterial);
-// fieldMesh.matrixAutoUpdate = false;
-// fieldMesh.position.set(0, 20.5, 0);
-// fieldMesh.updateMatrix();
-// fieldMesh.receiveShadow = true;
-// scene.add(fieldMesh);
+player.userData.speed = 0.5;
+player.userData.up = false;
+player.userData.down = false;
+player.userData.left = false;
+player.userData.right = false;
+
+scene.add(player);
 
 
 function init() {
@@ -89,9 +111,13 @@ function init() {
 
 };
 
+init();
+
 /*///////////////////////////////////////////////////////////////////*/
 
 function animate() {
+  movePlayer()
+
 
 
 
@@ -100,16 +126,129 @@ function animate() {
 /*///////////////////////////////////////////////////////////////////*/
 
 
-/*///////////////////////////////////////////////////////////////////*/
-
-
 renderer.setAnimationLoop((_) => {
-  controls.update();
+  //controls.update();
   animate();
   stats.update();
   renderer.render(scene, camera);
 });
 
 
+
+
+
+
+
 /*///////////////////////////////////////////////////////////////////*/
 
+// document.addEventListener('mousemove', onDocumentMouseMove);
+// document.addEventListener('mousedown', onDocumentMouseDown, false);
+
+document.onclick = function () {
+  document.querySelector('canvas').requestPointerLock();
+}
+
+document.addEventListener('pointerlockchange', lockStatusChange, false);
+
+function lockStatusChange() {
+
+  if (document.pointerLockElement === document.querySelector('canvas')) {
+    document.addEventListener("mousemove", updateCirclePosition, false);
+  }
+  else {
+    document.removeEventListener("mousemove", updateCirclePosition, false);
+  }
+}
+
+
+
+function updateCirclePosition(event) {
+  const x = event.movementX / screen.width;
+  const y = event.movementY / screen.height;
+
+  movementX += x * rotationSpeed;
+  movementY += y * rotationSpeed;
+
+  movementX = MathUtils.clamp(movementX, - 1, 1);
+  movementY = MathUtils.clamp(movementY, - 1, 1);
+
+
+}
+
+
+
+function onDocumentMouseDown(event) {
+  event.preventDefault();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  //player.userData.shoot = true;
+}
+
+
+
+
+
+function movePlayer() {
+  target.set(movementX, 0, movementY).normalize();
+  target.add(player.position);
+  player.lookAt(target);
+
+  if (player.userData.up) {
+    player.position.z -= player.userData.speed;
+  }
+  if (player.userData.down) {
+    player.position.z += player.userData.speed;
+  }
+  if (player.userData.left) {
+    player.position.x -= player.userData.speed;
+  }
+  if (player.userData.right) {
+    player.position.x += player.userData.speed;
+  }
+}
+
+
+addEventListener("keydown", onkeydown, false);
+addEventListener("keyup", onkeyup, false);
+
+function onkeydown(event) {
+  switch (event.keyCode) {
+    case 38: // up
+    case 87: // w
+      player.userData.up = true;
+      break;
+    case 37: // left
+    case 65: // a
+      player.userData.left = true;
+      break;
+    case 40: // down
+    case 83: // s
+      player.userData.down = true;
+      break;
+    case 39: // right
+    case 68: // d
+      player.userData.right = true;
+      break;
+  }
+}
+
+function onkeyup(event) {
+  switch (event.keyCode) {
+    case 38: // up
+    case 87: // w
+      player.userData.up = false;
+      break;
+    case 37: // left
+    case 65: // a
+      player.userData.left = false;
+      break;
+    case 40: // down
+    case 83: // s
+      player.userData.down = false;
+      break;
+    case 39: // right
+    case 68: // d
+      player.userData.right = false;
+      break;
+  }
+}
